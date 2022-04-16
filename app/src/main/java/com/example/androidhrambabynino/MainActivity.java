2a71b5b00ae610494de47b940a22b0a1eee4a69b
gorbulevsv <gorbulevsv@yandex.ru>
2022-04-16 12:03:40 +0300

    1

--- app/src/main/java/com/example/androidhrambabynino/MainActivity.java
+++ app/src/main/java/com/example/androidhrambabynino/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         drawer = findViewById(R.id.main_layout);
         drawer.addDrawerListener(toggler);
 
-        toggler = setupDrawerToggle();
+        toggler = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
         toggler.setDrawerIndicatorEnabled(true);
         toggler.syncState();
 
@@ -61,62 +61,49 @@ public class MainActivity extends AppCompatActivity {
     private void selectDrawerItem(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_posts:
-                //toolbar.setTitle("Объявления");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/");
                 break;
             case R.id.nav_anons:
-                //toolbar.setTitle("Объявления");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/объявления");
                 break;
             case R.id.nav_news:
-                //toolbar.setTitle("Новости");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/новости");
                 break;
             case R.id.nav_video:
-                //toolbar.setTitle("Видео");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/видео");
                 break;
             case R.id.nav_for_kliros:
-                //toolbar.setTitle("Для клироса");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/для клироса");
                 break;
             case R.id.nav_life:
-                //toolbar.setTitle("Жития святых");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/жития святых");
                 break;
             case R.id.nav_our_chirch:
-                //toolbar.setTitle("О нашем храме");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/о нашем храме");
                 break;
             case R.id.nav_tainstva:
-                //toolbar.setTitle("О таинствах");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/о таинствах");
                 break;
             case R.id.nav_raznoe:
-                //toolbar.setTitle("Разное");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts/разное");
                 break;
 
             case R.id.nav_schedule:
-                //toolbar.setTitle("Расписание богослужений");
                 webview.loadUrl("https://hram-babynino.somee.com/android/schedule");
                 break;
             case R.id.nav_photos:
-                //toolbar.setTitle("Фотографии");
                 webview.loadUrl("https://hram-babynino.somee.com/android/photos");
                 break;
             case R.id.nav_contacts:
-                //toolbar.setTitle("Контакты");
                 webview.loadUrl("https://hram-babynino.somee.com/android/contacts");
                 break;
             default:
-                //toolbar.setTitle("Объявления");
                 webview.loadUrl("https://hram-babynino.somee.com/android/posts");
+                break;
         }
 
-        // Выделение выбранного элемента было выполнено с помощью NavigationView
+        toolbar.setTitle(item.getTitle().toString().replace("  ",""));
         item.setChecked(true);
-        // Закройте навигационный ящик
         drawer.closeDrawers();
     }
 
@@ -128,30 +115,12 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    private ActionBarDrawerToggle setupDrawerToggle() {
-        return new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
-    }
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (toggler.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Синхронизируйте состояние переключения после того, как произошло onRestoreInstanceState.
-        toggler.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Передайте любое изменение конфигурации переключателям ящика
-        toggler.onConfigurationChanged(newConfig);
     }
 
     @Override
